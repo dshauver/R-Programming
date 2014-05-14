6392a0e668faeb68c98aaa6ed9d4c613fe9c8e44
dshauver <dshauver@gmail.com>
2014-05-14 11:01:57 -0500

    modified pollutantmean.R - removed some code validation points & comments, un-neccessary code

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,20 +1,14 @@
-pollutantmean <- function(directory, pollutant, id = 1:332) {
+################################################################################
+pollutantmean <- function(directory, pollutant, id = 1:332, naRem = TRUE) {
         ## 'directory' is a character vector of length 1 indicating
         ## the location of the CSV files
 
 	##Not checking to see if directory is a valid path.  
-	##getwd() should be the parent of "directory"
-	##or absolute path should be used.
-
-	##Validating input 1
-	##print(directory)
+	##getwd() should be the parent of "directory" or use absolute path.
 
         ## 'pollutant' is a character vector of length 1 indicating
         ## the name of the pollutant for which we will calculate the
         ## mean; either "sulfate" or "nitrate".
-
-	##Validating input 2
-	##print(pollutant)
 
         ## 'id' is an integer vector indicating the monitor ID numbers
         ## to be used
@@ -22,21 +16,15 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
 	## initial component of file name.  Need to handle range of ID
 	## numbers, pattern match for input files.  i.e. 1 = 001.csv.
 
-	##Validating input 3
-	##print(id)
+	## Filenames are 3 digits - 001 through 332.  Force id to 3 digits
+	id <- sprintf("%03d", id)
 
-	##Create a filename to be used for import
-	##filename <- paste(directory,(paste(id,"csv",sep = ".")),sep = "/")
-	##Validating operation
-	##print(filename)
-
-	##Read in the dataset
-	##rawdata <- read.csv(filename)
-	##Clean the data
-	##dataset <- rawdata[(complete.cases(rawdata)), ]
-
-	##Elegant - read in data with lines containing "NA" omitted
+	## Assume sensor failure or adverse environmental conditions on partial readings.
+	## Ignore incomplete readings
 	dataset <- na.omit(read.csv(paste(directory,(paste(id,"csv",sep = ".")),sep = "/")))
+
+	## Read in entire dataset, including incomplete/NA portions.
+	## dataset <- read.csv(paste(directory,(paste(id,"csv",sep = ".")),sep = "/"))
 
 	##Validating operation
 	##dataset
@@ -45,14 +33,10 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
         ## Return the mean of the pollutant across all monitors list
         ## in the 'id' vector (ignoring NA values)
 
-	##Ignored all incomplete datasets above ; this implies that we're
-	##looking for the mean across all datasets, even if incomplete.
-	##Have confirmed that in at least 1 dataset (001.csv) :
-	##	mean(rawdata$nitrate, na.rm = TRUE)
-	##	does not equal
-	##	mean(dataset$nitrate) when na.omit is used on file read.
-	
 	##requires mean(dataset$pollutant), where pollutant is evaluated.
 	mean(dataset[[pollutant]])
+
+	##Below variant for use if we don't clean incomplete datasets on read.
+	##mean(dataset[[pollutant]], na.rm = naRem)
 
 }
